5704c623f173d2bc910e43c4646291e96b67d9e0
Reed James <reedtjames1234@gmail.com>
2019-07-12 12:32:40 -0500

    MVP complete

--- starwars/src/App.js
+++ starwars/src/App.js
@@ -25,7 +25,7 @@ const App = () => {
 
   return (
     <div className="App">
-      <h1 className="Header">React Wars Test Test</h1>
+      <h1 className="Header">React Wars Test </h1>
       <CharList char={char} />
     </div>
   );
---
+++ starwars/src/CharList.test.js
@@ -0,0 +1,18 @@
+// import React from "react";
+// import ReactDOM from "react-dom";
+// import { CharList } from "./components/CharList";
+// import renderer from "react-test-renderer";
+
+// it("renders without crashing", () => {
+//   const div = document.createElement("div");
+//   ReactDOM.render(<CharList />, div);
+//   ReactDOM.unmountComponentAtNode(div);
+// });
+
+// describe("<CharList />", () => {
+//   it("should match snapshot", () => {
+//     const tree = renderer.create(<CharList />).toJSON();
+
+//     expect(tree).toMatchSnapshot();
+//   });
+// });
